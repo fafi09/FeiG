1616c818c5970b9ad976d3677c4e5fc0928b43d9
fafi09 <fafi09@126.com>
2018-08-10 17:11:29 +0800

    v9.0追加文件发送

--- FeiG/ChatView.cpp
+++ FeiG/ChatView.cpp
@@ -25,10 +25,16 @@ void CChatView::DoDataExchange(CDataExchange* pDX)
 	CFormView::DoDataExchange(pDX);
 	DDX_Control(pDX, IDC_CHAT_LIST, m_wndChatList);
 	DDX_Control(pDX, IDC_CHAT_CONTENT, m_wndChatData);
+	DDX_Control(pDX, IDC_LIST_FILE, m_wndFileList);
+	DDX_Control(pDX, IDC_STATIC_ZILIAO, m_wndStaticZiLiao);
 }
 
 BEGIN_MESSAGE_MAP(CChatView, CFormView)
 	ON_BN_CLICKED(IDC_SEND_BTN, &CChatView::OnBnClickedSendBtn)
+	ON_BN_CLICKED(IDC_BUTTON1, &CChatView::OnBnClickedButton1)
+	ON_WM_CREATE()
+	ON_BN_CLICKED(IDC_BUTTON_FILEDLG, &CChatView::OnBnClickedButtonFiledlg)
+	ON_BN_CLICKED(IDC_BUTTON_CANCEL, &CChatView::OnBnClickedButtonCancel)
 END_MESSAGE_MAP()
 
 
@@ -83,7 +89,82 @@ void CChatView::AddChat(CString strChat, CString strIP)
 	CTime tmCur = CTime::GetCurrentTime();
 	CString strTime = tmCur.Format("%Y-%m-%d %H:%M:%S");
 
-	CString strData = strIP + TEXT(" ") + strTime + TEXT(":") + strChat;
+	CString strData = strIP + TEXT(" ") + strTime + TEXT(":");
 	int nItem = m_wndChatList.AddString(strData);
+	m_wndChatList.AddString(strChat);
 	m_wndChatList.SetCurSel(nItem);
+}
+
+
+void CChatView::OnBnClickedButton1()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
+
+
+int CChatView::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CFormView::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	// TODO:  在此添加您专用的创建代码
+	
+	
+	return 0;
+}
+
+
+void CChatView::OnInitialUpdate()
+{
+	CFormView::OnInitialUpdate();
+
+	LVCOLUMN col;
+
+	BOOL isExist = m_wndFileList.GetColumn(0, &col);
+
+	if(isExist == FALSE)
+	{
+		m_wndFileList.InsertColumn(0,TEXT("名称"),LVCFMT_LEFT, 100, -1);
+		m_wndFileList.InsertColumn(1,TEXT("类型"),LVCFMT_LEFT, 100, -1);
+		m_wndFileList.InsertColumn(2,TEXT("大小"),LVCFMT_LEFT, 100, -1);
+		m_wndFileList.InsertColumn(3,TEXT("路径"),LVCFMT_LEFT, 100, -1);
+	}
+	
+	// TODO: 在此添加专用代码和/或调用基类
+}
+
+
+void CChatView::OnBnClickedButtonFiledlg()
+{
+	CFileDialog fileDlg(TRUE);
+
+	if(fileDlg.DoModal() == IDOK)
+	{
+		CString strFileName = fileDlg.GetFileName();
+		int nItem = m_wndFileList.InsertItem(0, strFileName);
+
+		//打开文件
+		CFile file;
+		CString strFileSize;
+		
+		if ( !file.Open( strFileName, CFile::modeReadWrite ) )
+		{
+			return;
+		}
+		else
+		{
+			strFileSize.Format(TEXT("%ld"),file.GetLength());
+			m_wndFileList.SetItemText(nItem, 1, TEXT("文件"));
+			m_wndFileList.SetItemText(nItem, 2,  strFileSize);
+			m_wndFileList.SetItemText(nItem, 3, file.GetFilePath());
+		}
+
+	}
+	
+}
+
+
+void CChatView::OnBnClickedButtonCancel()
+{
+	m_wndFileList.DeleteAllItems();
 }
--- FeiG/ChatView.h
+++ FeiG/ChatView.h
@@ -2,6 +2,7 @@
 #include "afxwin.h"
 #include "resource.h"
 #include "UserClient.h"
+#include "afxcmn.h"
 
 // CChatView ¥∞ÃÂ ”Õº
 
@@ -32,6 +33,13 @@ public:
 	afx_msg void OnBnClickedSendBtn();
 	CUserClient* m_pUserClient;
 	void AddChat(CString strChat, CString strIP);
+	afx_msg void OnBnClickedButton1();
+	CListCtrl m_wndFileList;
+	CStatic m_wndStaticZiLiao;
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	virtual void OnInitialUpdate();
+	afx_msg void OnBnClickedButtonFiledlg();
+	afx_msg void OnBnClickedButtonCancel();
 };
 
 
